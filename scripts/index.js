5b7a4acf454b33479af7886dd64e9597e2cdb731
pedro-makoski <148820090+pedro-makoski@users.noreply.github.com>
2024-06-12 16:15:16 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,11 @@ const sinais = ['+', '-']
 const operacoes_reais = ['+', '-', '/', '*', '*', '.']
 const numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
+
+if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i)|| navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)){
+    input.style.userSelect = 'none'
+}
+
 function eval_melhor(valor) {
     lista_a_partir_de_string = Array.from(valor)
     const index_especiais = []
